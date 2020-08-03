caaa07939f50816aacb7355aecfc6608da96a17f
yatender-oktalk <yatender.nitk@gmail.com>
2020-08-03 11:15:46 +0530

    Adding simple constructor

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import web3 from './web3';
+import React, { Component } from "react";
+import "./App.css";
+import web3 from "./web3";
+import lottery from "./lottery";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { manager: "", players: [] };
+  }
+
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call({ from: manager });
+    this.setState({ manager, players });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>This contract is managed by {this.state.manager}</p>
+        <p>This contract is having players {this.state.players}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
